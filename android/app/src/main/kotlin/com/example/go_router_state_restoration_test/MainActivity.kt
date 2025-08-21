deb04885098ea8a875e48b9bf139f0e4c328c082
LukasMirbt <lukas.mirbt@hotmail.com>
2025-08-21 18:53:32 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/go_router_state_restoration_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.go_router_state_restoration_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:go_router_state_restoration_test/app.dart';
+
+void main() {
+  runApp(const App());
+}
